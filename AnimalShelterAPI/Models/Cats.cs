228e3ae42bd3d5ced766c04fb8b6de9f2abcbcb1
Joey3001 <josephnero111@gmail.com>
2021-01-22 09:32:58 -0800

    Add Cat.cs to Models.

---
+++ AnimalShelterAPI/Models/Cats.cs
@@ -0,0 +1,10 @@
+namespace AnimalShelterAPI.Models
+{
+  public class Cat
+  {
+    public int CatId { get; set; }
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+  }
+}
